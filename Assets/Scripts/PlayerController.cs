a67fb68e9ac42090a6c1a528fb90df38ec35bdca
David GB <davidnew33@gmail.com>
2021-02-07 22:34:01 +0100

    + Jump completado. + Mejora de colliderr del suelo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] 
+    private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
+    [Range(0, .4f)][SerializeField] 
+    private float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    [SerializeField]
+    public GameObject topeSuelo;
+
+    [SerializeField]
+    public float speed = 5f;
+    [SerializeField]
+    public float jumpForce = 5f;
+
+    [Range(0, .3f)] [SerializeField] 
+    private float movementSmoothing = .05f;  // How much to smooth out the movement
+
+    private Animator m_Animator;
+    private Rigidbody2D m_Rigidbody2D;
+    private SpriteRenderer m_SpriteRenderer;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private bool jumpInput;
+    private bool stillPressing = true;
+
+    [SerializeField] 
+    private LayerMask m_WhatIsGround;  
+    private bool isGrounded; 
+
+    private float baseGravityScale;
+
+    private Vector3 m_Velocity = Vector3.zero;
+
+    /*[Space]
+    [Header("Events")]
+    [Space]
+    // Evento de cuando caes al suelo.
+    public UnityEvent OnLandEvent;
+    public UnityEvent OnAirEvent;
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        baseGravityScale = m_Rigidbody2D.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        horizontalInput = Input.GetAxisRaw ("Horizontal");
+        verticalInput = Input.GetAxisRaw ("Vertical"); //Useless for now
+
+        //If horizontalInput is negative, dale la vuelta.
+        if (horizontalInput > 0) 
+        {
+            horizontalInput = 1;
+            m_SpriteRenderer.flipX = true;
+        }
+        else if (horizontalInput < 0)
+        {
+            horizontalInput = -1;
+            m_SpriteRenderer.flipX = false;
+        }
+
+        //Movement
+        Vector3 targetVelocity = new Vector2(horizontalInput * speed, m_Rigidbody2D.velocity.y);
+        // And then smoothing it out and applying it to the character
+        m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, movementSmoothing);
+
+        //Jumping
+        jumpInput = Input.GetButton ("Jump");
+
+        if(isGrounded && jumpInput)
+        {
+            stillPressing = true;
+            isGrounded = false;
+            m_Rigidbody2D.AddForce(new Vector3(0,1,0) * jumpForce, ForceMode2D.Impulse);
+        } else if(stillPressing && !jumpInput) //Soltamos el botón o se acaba el tiempo
+        {
+            stillPressing = false;
+            m_Rigidbody2D.gravityScale = baseGravityScale;
+            if(m_Rigidbody2D.velocity.y > 0) m_Rigidbody2D.velocity = new Vector2( m_Rigidbody2D.velocity.x, 0f);
+        }
+
+        //Update values on Animator
+        m_Animator.SetFloat("speed", horizontalInput);
+        m_Animator.SetBool("jumping", !isGrounded);
+    }
+
+    void FixedUpdate()
+    {
+        
+        bool wasGrounded = isGrounded;
+        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                isGrounded = true;
+            } 
+        }
+    }
+}
